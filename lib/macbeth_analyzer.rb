493cc04c1341ac1a8e86f33ff5dd3b5e9d9033b5
Mayra Cabrera <mcabrera1087@gmail.com>
2015-09-13 17:16:05 -0500

    Fix identation among files

--- lib/macbeth_analyzer.rb
+++ lib/macbeth_analyzer.rb
@@ -3,47 +3,47 @@ require 'nokogiri'
 require 'pry'
 
 class MacbethAnalyzer 
-	attr_reader :doc, :speakers, :number_of_lines
+  attr_reader :doc, :speakers, :number_of_lines
 
-	def initialize
-		@doc = Nokogiri::HTML(open(biblio_url))
-		@speakers = []
-		@number_of_lines = {}
-	end
-
-	def run
-		search_for_speakers
-		count_lines_for_speakers
-		print_results
-	end
-
-	def search_for_speakers
-		@speakers = speakers_from_biblio.uniq! 
-		@speakers -= ["ALL"]
+  def initialize
+    @doc = Nokogiri::HTML(open(biblio_url))
+    @speakers = []
+    @number_of_lines = {}
   end
 
-	def count_lines_for_speakers
-		@speakers.each do |speaker|
-			lines = @doc.xpath("//speech//speaker[contains(text(), '#{speaker}')]").count			
-			@number_of_lines[speaker] = lines
-		end
-	end
+  def run
+    search_for_speakers
+    count_lines_for_speakers
+    print_results
+  end
 
-	def print_results
-		@number_of_lines.each do |speaker, lines|
-			p "#{lines} #{speaker}"
-		end
-	end
+  def search_for_speakers
+    @speakers = speakers_from_biblio.uniq! 
+    @speakers -= ["ALL"]
+  end
 
-	private
+  def count_lines_for_speakers
+    @speakers.each do |speaker|
+      lines = @doc.xpath("//speech//speaker[contains(text(), '#{speaker}')]").count     
+      @number_of_lines[speaker] = lines
+    end
+  end
 
-	def biblio_url
-		"http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
-	end
+  def print_results
+    @number_of_lines.each do |speaker, lines|
+      p "#{lines} #{speaker}"
+    end
+  end
 
-	def speakers_from_biblio
-		@doc.css("speech speaker").map(&:text)
-	end	
+  private
+
+  def biblio_url
+    "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
+  end
+
+  def speakers_from_biblio
+    @doc.css("speech speaker").map(&:text)
+  end 
 end
 
 analyzer = MacbethAnalyzer.new
--- spec/macbeth_analyzer_spec.rb
+++ spec/macbeth_analyzer_spec.rb
@@ -2,52 +2,52 @@ require './lib/macbeth_analyzer'
 
 describe MacbethAnalyzer do
 
-	describe "initialize" do
-		before(:each) do
-			@analyzer = MacbethAnalyzer.new
-		end
+  describe "initialize" do
+    before(:each) do
+      @analyzer = MacbethAnalyzer.new
+    end
 
-		it "should initialize a nokogiri document" do
-			expect(@analyzer.doc).to be_a_kind_of(Nokogiri::HTML::Document)
-		end
-		
-		it "should set character list as an empty array" do
-			expect(@analyzer.speakers).to eq([])
-		end
+    it "should initialize a nokogiri document" do
+      expect(@analyzer.doc).to be_a_kind_of(Nokogiri::HTML::Document)
+    end
+    
+    it "should set character list as an empty array" do
+      expect(@analyzer.speakers).to eq([])
+    end
 
-		it "should set number of lines as an empty hash" do
-			expect(@analyzer.number_of_lines).to eq({})
-		end
-	end
+    it "should set number of lines as an empty hash" do
+      expect(@analyzer.number_of_lines).to eq({})
+    end
+  end
 
-	describe "#search_for_speakers" do
-		before(:all) do
-			@analyzer = MacbethAnalyzer.new
-			@analyzer.search_for_speakers
-		end
+  describe "#search_for_speakers" do
+    before(:all) do
+      @analyzer = MacbethAnalyzer.new
+      @analyzer.search_for_speakers
+    end
 
-		it "should search for macbeth characters" do
-			expect(@analyzer.speakers).to_not be_empty
-		end
+    it "should search for macbeth characters" do
+      expect(@analyzer.speakers).to_not be_empty
+    end
 
-		it "should remove ALL entry from list" do
-			expect(@analyzer.speakers.include? "ALL").to be_falsy
-		end
-	end
+    it "should remove ALL entry from list" do
+      expect(@analyzer.speakers.include? "ALL").to be_falsy
+    end
+  end
 
-	describe "#count_lines_for_speakers" do
-		before(:all) do
-			@analyzer = MacbethAnalyzer.new
-			@analyzer.search_for_speakers
-			@analyzer.count_lines_for_speakers
-		end
+  describe "#count_lines_for_speakers" do
+    before(:all) do
+      @analyzer = MacbethAnalyzer.new
+      @analyzer.search_for_speakers
+      @analyzer.count_lines_for_speakers
+    end
 
-		it "should set number of lines per character" do
-			@analyzer.speakers.each do |speaker|
-				expect(@analyzer.number_of_lines[speaker]).to_not be_nil
-			end
-		end
-	end
+    it "should set number of lines per character" do
+      @analyzer.speakers.each do |speaker|
+        expect(@analyzer.number_of_lines[speaker]).to_not be_nil
+      end
+    end
+  end
 end
 
 
